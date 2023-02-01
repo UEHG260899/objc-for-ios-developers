af7d5436f12a8c72bc64a660452dc78b4629938f
Uriel Enrique <urielenrique99@gmail.com>
2023-01-31 18:54:36 -0600

    ClassAnatomy: Initial Commit

---
+++ ClassAnatomy/ClassAnatomy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ClassAnatomy
+//
+//  Created by Uriel Hernandez Gonzalez on 31/01/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ClassAnatomy/ClassAnatomy/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  ClassAnatomy
+//
+//  Created by Uriel Hernandez Gonzalez on 31/01/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
