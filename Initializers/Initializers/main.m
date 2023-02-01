c9076ff147e32ce66989529b5ad9848327ad355f
Uriel Enrique <urielenrique99@gmail.com>
2023-02-01 12:42:46 -0600

    Initializers: Initial commit

---
+++ Initializers/Initializers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Initializers
+//
+//  Created by Uriel Hernandez Gonzalez on 01/02/23.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
