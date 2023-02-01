105d7d0ca8d651d5cb40be28b24ae2c420eea0b4
Uriel Enrique <urielenrique99@gmail.com>
2023-01-31 18:53:49 -0600

    HelloObjc: Project done

--- HelloObjc/HelloObjc/ViewController.m
+++ HelloObjc/HelloObjc/ViewController.m
@@ -18,5 +18,16 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onClickBaitClicked:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Tell us about you" message:@"Do you like pizza?" preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction *yesAction = [UIAlertAction actionWithTitle:@"YES" style:UIAlertActionStyleDefault handler:nil];
+    UIAlertAction *closeAction = [UIAlertAction actionWithTitle:@"CLOSE" style:UIAlertActionStyleCancel handler:nil];
+    
+    [alert addAction:yesAction];
+    [alert addAction:closeAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
