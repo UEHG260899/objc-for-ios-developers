2ebee919728a1a8914f3a73f567d559bf574c963
Uriel Enrique <urielenrique99@gmail.com>
2023-01-31 19:12:10 -0600

    ClassAnatomy: Final Commit

--- ClassAnatomy/ClassAnatomy/ViewController.m
+++ ClassAnatomy/ClassAnatomy/ViewController.m
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(nonatomic, strong) NSString *vehicle;
 @end
 
 @implementation ViewController
@@ -16,7 +16,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSLog(@"Name: %@", self.name);
+    self.name = @"Uriel";
+    NSLog(@"Name: %@", self.name);
+    
+    _name = @"Enrique";
+    
+    NSLog(@"Name: %@", _name);
+    
+    [self setName:@"Jose"];
+    
+    NSLog(@"Name: %@", [self name]);
 }
-
 
 @end
