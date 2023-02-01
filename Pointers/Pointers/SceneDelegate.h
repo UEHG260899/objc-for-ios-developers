1cccbb8933a8deb3474cabfe1ab8ede987e9c803
Uriel Enrique <urielenrique99@gmail.com>
2023-02-01 09:21:20 -0600

    Pointers initial commit

---
+++ Pointers/Pointers/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Pointers
+//
+//  Created by Uriel Hernandez Gonzalez on 01/02/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Pointers/Pointers/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Pointers
+//
+//  Created by Uriel Hernandez Gonzalez on 01/02/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
