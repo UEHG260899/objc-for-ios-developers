5a13760456deee6bb7496f4aa2c02fb046d7bf37
Uriel Enrique <urielenrique99@gmail.com>
2023-02-01 09:57:37 -0600

    Strings: final commit

--- Strings/Strings/ViewController.m
+++ Strings/Strings/ViewController.m
@@ -16,6 +16,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    NSString *firstName = @"Uriel";
+    
+    NSLog(@"First Name: %@", firstName);
+    
+    NSString *fullName = [NSString stringWithFormat:@"%@ Hernandez", firstName];
+    
+    NSString *fullNameWithAppending = [fullName stringByAppendingString:@" Hernandez"];
+    
+    NSLog(@"Full Name: %@", fullName);
+    
+    NSString *var1 = @"Junk";
+    NSString *var2 = @"in the trunk";
+    
+    if ([var1 isEqualToString:var2]) {
+        NSLog(@"They are equal");
+    }
+    
+    if ([var1 caseInsensitiveCompare:var2] == NSOrderedSame) {
+        
+    }
 }
 
 
