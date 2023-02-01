2bc3428b1dc77ee66c65753d42b92909905b0ebc
Uriel Enrique <urielenrique99@gmail.com>
2023-02-01 11:17:32 -0600

    Conditionals: Initial commit

---
+++ Conditionals/Conditionals/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Conditionals
+//
+//  Created by Uriel Hernandez Gonzalez on 01/02/23.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
