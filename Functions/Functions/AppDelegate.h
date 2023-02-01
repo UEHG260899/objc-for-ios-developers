e7b285f00fe08ef3ca778bfcb83c2750cbc3ea8a
Uriel Enrique <urielenrique99@gmail.com>
2023-02-01 11:24:50 -0600

    Functions: Initial Commit

---
+++ Functions/Functions/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Functions
+//
+//  Created by Uriel Hernandez Gonzalez on 01/02/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
