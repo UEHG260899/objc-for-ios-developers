fb836f4ddb4510891492860c4e94a13bd400832b
Uriel Enrique <urielenrique99@gmail.com>
2023-01-31 19:41:53 -0600

    Getters: Initial Commit

---
+++ Getters/Getters/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Getters
+//
+//  Created by Uriel Hernandez Gonzalez on 31/01/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Getters/Getters/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Getters
+//
+//  Created by Uriel Hernandez Gonzalez on 31/01/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
